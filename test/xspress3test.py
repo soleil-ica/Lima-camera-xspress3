75afc25ae28575bfb172038eaf4ec7e5e610814c
Stu Fisher <stu@q3f.org>
2017-03-02 12:19:36 +0100

    add up to date test scripts

---
+++ test/xspress3test.py
@@ -0,0 +1,43 @@
+# Xspress3 Test 1
+# No file saving
+
+import PyTango
+import numpy
+import time
+import random
+
+# definitions
+nframes = 10
+exp_time = 1 # seconds
+
+dev = PyTango.DeviceProxy('lima/xspress3/1')
+lima = PyTango.DeviceProxy('lima/limaccd/1')
+
+dev.set_timeout_millis(30000)
+
+lima.write_attribute("acq_nb_frames",nframes)
+lima.write_attribute("acq_expo_time", exp_time)
+
+# INTERNAL = Internal trigger
+# EXTERNAL = TTL Veto (input TTL1)
+#lima.write_attribute("acq_trigger_mode", "EXTERNAL_GATE")
+lima.write_attribute("acq_trigger_mode", "INTERNAL_TRIGGER")
+lima.command_inout("prepareAcq")
+lima.command_inout("startAcq")
+
+lastFrame = -1
+channel = 0;
+
+while dev.read_attribute("acqRunning").value :
+    try:
+        time.sleep(0.5)
+        currentFrame=lima.read_attribute("last_image_ready").value
+        if currentFrame > lastFrame:
+            lastFrame = currentFrame
+            data = dev.command_inout("ReadScalers",[lastFrame, channel])
+            print "lastFrame", lastFrame, "allevent" ,data[3],"allgood",data[4], "dt%", data[9], "dtf", data[10]
+            hdata = dev.command_inout("ReadHistogram",[lastFrame, channel])
+            print "hist data ",hdata
+
+    except (KeyboardInterrupt, SystemExit):
+        lima.command_inout("stopAcq")
---
+++ test/xspress3test2.py
@@ -0,0 +1,51 @@
+# Xspress3 Test 2
+# With file saving
+
+import PyTango
+import numpy
+import time
+import random
+
+# definitions
+nframes = 10
+exp_time = 1 # seconds
+
+dev = PyTango.DeviceProxy('lima/xspress3/1')
+lima = PyTango.DeviceProxy('lima/limaccd/1')
+
+# do not change the order of the saving attributes!
+lima.write_attribute("saving_directory","/home/xspress3/tango/data")
+lima.write_attribute("saving_format","HDf5")
+lima.write_attribute("saving_overwrite_policy","Abort")
+lima.write_attribute("saving_suffix", ".hdf")
+lima.write_attribute("saving_prefix","xsp3_")
+lima.write_attribute("saving_mode","Auto_Frame")
+lima.write_attribute("saving_managed_mode","SOFTWARE")
+lima.write_attribute("saving_frame_per_file", nframes)
+
+
+dev.set_timeout_millis(30000)
+
+lima.write_attribute("acq_nb_frames",nframes)
+lima.write_attribute("acq_expo_time", exp_time)
+#lima.write_attribute("acq_trigger_mode", "EXTERNAL_GATE")
+lima.write_attribute("acq_trigger_mode", "INTERNAL_TRIGGER")
+lima.command_inout("prepareAcq")
+lima.command_inout("startAcq")
+
+lastFrame = -1
+channel = 0;
+
+while dev.read_attribute("acqRunning").value :
+    try:
+        time.sleep(0.5)
+        currentFrame=lima.read_attribute("last_image_ready").value
+        if currentFrame > lastFrame:
+            lastFrame = currentFrame
+            data = dev.command_inout("ReadScalers",[lastFrame, channel])
+            print "lastFrame", lastFrame, "allevent" ,data[3],"allgood",data[4], "dt%", data[9], "dtf", data[10]
+            hdata = dev.command_inout("ReadHistogram",[lastFrame, channel])
+            print "hist data ",hdata
+
+    except (KeyboardInterrupt, SystemExit):
+        lima.command_inout("stopAcq")
